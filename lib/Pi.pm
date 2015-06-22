d0feb76ef5067a3109f044f6b33539de55848263
leetom <leetom1991@163.com>
2015-06-22 20:21:15 +0800

    init

---
+++ lib/Pi.pm
@@ -0,0 +1,19 @@
+package Pi;
+use Mojo::Base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Documentation browser under "/perldoc"
+  $self->plugin('PODRenderer');
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get('/music')->to('example#music');
+  $r->get('/stop')->to('example#stop');
+}
+
+1;
---
+++ lib/Pi/Example.pm
@@ -0,0 +1,36 @@
+package Pi::Example;
+use Mojo::Base 'Mojolicious::Controller';
+
+# This action will render a template
+sub welcome {
+  my $self = shift;
+
+  # Render template "example/welcome.html.ep" with message
+  $self->render(
+    message => 'Welcome to the Mojolicious real-time web framework!');
+}
+
+sub music {
+    my $self = shift;
+    my $id = $self->param('track');
+
+    if($id){
+        # there is sound to play
+        my @arg = ("/home/leetom/Music/player.pl", $id);
+        system(@arg) == 0 or die("error play file @arg");
+
+        $self->redirect_to("/music");
+    }else{
+        $self->render();
+    }
+}
+
+sub stop {
+    my $self = shift;
+
+    system("pkill mpg123");
+
+    $self->redirect_to("/music");
+}
+
+1;
